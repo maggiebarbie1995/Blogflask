89d0ff9b052051fbbbd2d0cb62bfc5e27520e027
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-25 04:47:34 +0300

    add routing to transform content to integer to return response to edit_post.html

--- app/main/views.py
+++ app/main/views.py
@@ -116,3 +116,20 @@ def post(id):
     comments = pagination.items
     return render_template('post.html', posts=[post], form=form,
                             comments=comments, pagination=pagination)
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+            not current_user.can(Permission.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
+
