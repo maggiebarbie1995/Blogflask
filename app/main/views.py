1b652851a06323ea539313df0b5f748267cb39d6
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-25 04:33:05 +0300

    add main route to return response to edit_profile.html

--- app/main/views.py
+++ app/main/views.py
@@ -43,7 +43,6 @@ def index():
     return render_template('index.html', form=form, posts=posts,
                             show_followed=show_followed, pagination=pagination)
 
-
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
@@ -53,4 +52,20 @@ def user(username):
         error_out=False)
     posts = pagination.items
     return render_template('user.html', user=user, posts=posts, pagination=pagination)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)   
 
